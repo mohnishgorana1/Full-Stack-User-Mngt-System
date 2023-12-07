871695a80819cd546ed8123911c5e7040a3f689f
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-07 14:20:58 +0530

    db connection changed in app file and routing setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,20 @@
 import express from "express";
+import connectToDatabase from './config/dbConfig.js'
 
 const app = express();
+
+
+// db connection
+connectToDatabase();
+
+// application middlewares
+
+
+// import routing
+import authRouter from './routes/auth.route.js'
+
+
+
 
 app.use("/", (req, res) => {
   res.status(200).json({
