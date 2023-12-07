2503cffd12b1299ffbd0377d118e9d6d9e6d2954
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-07 14:19:29 +0530

    init

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { signup } from '../controller/auth.controller';
+
+
+const authRouter = express.Router();
+
+authRouter.post('/signup', signup);
+
+export default authRouter
