760af6bb160f2a3b6d458487075b3f3e9c75deb2
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-07 14:12:46 +0530

    database connection init

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -1,1 +1,13 @@
-const
+import mongoose from "mongoose";
+
+const MONGODB_URL = String(process.env.MONGODB_URL);
+
+
+const connectToDatabase = async () => {
+  await mongoose
+    .connect(MONGODB_URL)
+    .then((conn) => console.log(`Connected to DB : ${conn.connection.host}`))
+    .catch((err) => console.log(`Error connecting to DB : ${err.message}`));
+};
+
+export default connectToDatabase;
