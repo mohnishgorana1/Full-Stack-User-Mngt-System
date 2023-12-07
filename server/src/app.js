ba3a588e1009b454e8f6c24c0fb04ec4b866826c
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-07 15:57:07 +0530

    add route config in app and add middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,20 @@
 import express from "express";
-import connectToDatabase from './config/dbConfig.js'
+import cookieParser from "cookie-parser";
+import connectToDatabase from "./config/dbConfig.js";
+
 
 const app = express();
-
 
 // db connection
 connectToDatabase();
 
 // application middlewares
-
+app.use(express.json()); // hey app i want to take json data from express server
+app.use(cookieParser());
 
 //*  routing
-import authRouter from './routes/auth.route.js'
-app.use('/api/auth', authRouter);
-
+import authRouter from "./routes/auth.route.js";
+app.use("/api/auth", authRouter);
 
 // default route
 app.use("/", (req, res) => {
@@ -21,6 +22,5 @@ app.use("/", (req, res) => {
     data: "JWT AUTH SERVER",
   });
 });
-
 
 export default app;
